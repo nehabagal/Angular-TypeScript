6248faf1e00ba664180f8cd99abe40dda452a887
nehabagal <nehabagal171@gmail.com>
2022-11-08 08:58:59 +0530

    variables in TS

---
+++ variable.js
@@ -0,0 +1,6 @@
+var n1 = 10;
+console.log(n1);
+var s1 = "you are the creater of your destiny";
+var s2 = "powr comes from powerful people";
+console.log(s1);
+console.log(s2);
---
+++ variable.ts
@@ -0,0 +1,7 @@
+var n1: number =10;
+console.log(n1);
+
+var s1: string = "you are the creater of your destiny";
+var s2: string = "powr comes from powerful people";
+console.log(s1);
+console.log(s2);
